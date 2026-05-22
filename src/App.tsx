5ec2f8010f5c29a08d615e1ba4bd6fad216d74d9
wlewis0991 <wlewis1990@gmail.com>
2026-05-22 07:36:54 -0700

    Navbar element added and routed to top of page

--- src/App.tsx
+++ src/App.tsx
@@ -4,23 +4,29 @@ import Onboarding from "./pages/Onboarding"
 import Auth from "./pages/Auth"
 import Account from "./pages/Account"
 import Profile from "./pages/Profile"
+import Navbar from "./components/Navbar"
 
 function App() {
 
   return (
     <>
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/onboarding" element={<Onboarding />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/auth/:pathname" element={<Auth />} />
-        <Route path="/account/:pathname" element={<Account />} />
-      </Routes>
+      <div className="min-h-screen flex flex-col">
+        <Navbar />
+          <main className="flex-1">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/onboarding" element={<Onboarding />} />
+              <Route path="/profile" element={<Profile />} />
+              <Route path="/auth/:pathname" element={<Auth />} />
+              <Route path="/account/:pathname" element={<Account />} />
+            </Routes>
+          </main>
+      </div>
     </BrowserRouter>
 
     </>
   )
 }
 
-export default App
+export default App  
