24992c168b7eb175545c971f49c506ce04d4f08a
wlewis0991 <wlewis1990@gmail.com>
2026-05-27 09:35:17 -0700

    Profile routes created in server

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,8 @@ import dotenv from "dotenv";
 import express from "express";
 import cors from "cors"
 import cookieParser from "cookie-parser"
+import { profileRouter } from "./routes/profile.routes";
+import { planRouter } from "./routes/plan.routes";
 
 dotenv.config();
 
@@ -11,6 +13,10 @@ const PORT = process.env.PORT || 3000
 app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
+
+//Routes
+app.use("/api/profile", profileRouter)
+app.use("/api/plan", planRouter)
 
 app.listen(PORT, () => {
     console.log(`🚀 Server is running on port: ${PORT}`)
